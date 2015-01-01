4ab02589676b7aa5f7fc4e8f3d998ee32e7c9c73
James Kolonusz <jamesak@gmail.com>
2014-12-31 16:52:56 -0800

    Add code to for when a added song is the only song in queue.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,27 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    // bind the function here to an event listener
+    this.on("add", this.enqueue, this);
+  },
+
+  enqueue : function(song) {
+    this.add(song);
+    if (this.length === 1){
+      this.playFirst()
+    }
+  },
+
+  playFirst : function(){
+    this.at(0).play()
+  },
+
+
+
+  // lengthOne : function(){
+  //   if (this.length === 1){
+  //     this.at(0).play()
+  //   }
+  // }
 
 });
